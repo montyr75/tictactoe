dbfa55c9fd968523dc4ff89be1587e20d8beccbc
Monty Rasmussen <montyr75@gmail.com>
2014-01-01 16:27:12 -0700

    updated observable model syntax

--- web/model/cell.dart
+++ web/model/cell.dart
@@ -2,7 +2,7 @@ library cell;
 
 import 'package:polymer/polymer.dart';
 
-class Cell extends Observable {
+class Cell extends Object with Observable {
   static const String EMPTY_CELL = "";
   static const String PLAYER_1 = "X";
   static const String PLAYER_2 = "O";
