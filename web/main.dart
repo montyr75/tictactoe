8af7bba5c17069b16b2606e69e9e1bbb101185b7
Monty Rasmussen <montyr75@gmail.com>
2016-07-29 16:31:35 -0600

    cleanup

--- web/main.dart
+++ web/main.dart
@@ -9,8 +9,9 @@ import 'package:angular2/platform/browser.dart';
 import 'package:tic_tac_toe/views/main_app/main_app.dart';
 import 'package:tic_tac_toe/services/logger_service.dart';
 
+const String APP_NAME = "tic_tac_toe";
 final bool _debugMode = window.location.host.contains('localhost');
-final LoggerService _log = new LoggerService(appName: "tic_tac_toe", debugMode: _debugMode);
+final LoggerService _log = new LoggerService(appName: APP_NAME, debugMode: _debugMode);
 
 main() async {
   await initPolymer();
