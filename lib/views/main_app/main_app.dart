0bf8b5bd4ae2b913b4ebed17697b5576edde91f0
Monty Rasmussen <montyr75@gmail.com>
2016-07-29 16:18:59 -0600

    cleanup

--- lib/views/main_app/main_app.dart
+++ lib/views/main_app/main_app.dart
@@ -1,5 +1,4 @@
 import 'package:angular2/angular2.dart';
-import 'package:logging/logging.dart';
 import 'package:polymer_elements/iron_flex_layout/classes/iron_flex_layout.dart';
 import 'package:polymer_elements/iron_icons.dart';
 import 'package:polymer_elements/paper_header_panel.dart';
